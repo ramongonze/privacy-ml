e873638fde5c483dc7cd1e72ae61dff308df81ba
Ramon G. Gonze <ramongonze@gmail.com>
2025-06-29 19:40:14 -0300

    Update attacks and privacy evaluation.

--- scripts/attacks.py
+++ scripts/attacks.py
@@ -2,143 +2,111 @@
 
 import numpy as np
 import pandas as pd
+import itertools as it
 from privacy_evaluation import model_inversion_acc
 from tqdm.auto import tqdm
+from datetime import datetime
 import os
+import pickle
 
-REPOSITORY_PATH = "/home/ramongonze/phd/privacy-ml" # privacy-ml repository path
+REPOSITORY_PATH = "/Users/ramongonze/phd/courses/privacidade_ml/privacy-ml" # privacy-ml repository path
 RESULTS_PATH = os.path.join(REPOSITORY_PATH, "results")
+log = lambda msg : print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {msg}")
 
-def attack(results_path, dataset_path, qids, target, sensitive, domain_sensitive, all_M:dict, all_A:dict, dp):
+def attack(dataset, qids, target, sensitive):
     """Measure the accuracy of the attack model A.
 
-        all_M (dict[str, object]): Dictionary with target model's name and the model object. The possible keys are "random_forest", "neural_network", "naive_bayes" and "logistic_regression".
-        all_A (dict[str, object]): Dictionary with attack model's name and the model object. The possible keys are "random_forest", "neural_network", "naive_bayes" and "logistic_regression".
+    Parameters:
+        dataset (str): Dataset, "adult" or "hospitals".
+        qids (list): List of QIDs.
+        target (str): Target attribute.
+        sensitive (str): Sensitive attribute.        
     """
+    # Original dataset path
+    dataset_path = os.path.join(REPOSITORY_PATH, f"data/{dataset}/{dataset}_pp.csv")
+    
+    results_path = os.path.join(RESULTS_PATH, "privacy_evaluation.csv")
+
     # Privacy parameters
     epsilons = [0.1, 0.5, 1, 10, 20, 50, 100]
 
     # Write header
-    with open(results_path, "w") as f:
-        f.write("target_model,attack_model,dp,epsilon,accuracy\n")
-         
-    for M_name, M in all_M.items(): # Target model
-        for A_name, A in all_A.items(): # Attack model
-            if dp:
-                for ep in epsilons:
-                    acc = model_inversion_acc(
-                        dataset_path=dataset_path,
-                        qids=qids,
-                        target=target,
-                        sensitive=sensitive,
-                        domain_sensitive=domain_sensitive,
-                        M=M,
-                        A=A,
-                        dp=dp,
-                        epsilon=ep
-                    )
-                    with open(results_path, "a") as f:
-                        f.write(f"{M_name},{A_name},{dp},{ep},{acc:.10f}\n")
-            else:
-                acc = model_inversion_acc(
-                    dataset_path=dataset_path,
-                    qids=qids,
-                    target=target,
-                    sensitive=sensitive,
-                    domain_sensitive=domain_sensitive,
-                    M=M,
-                    A=A,
-                    dp=dp
-                )
-                with open(results_path, "a") as f:
-                    f.write(f"{M_name},{A_name},False,None,{acc:.10f}\n")
+    write_header = not os.path.exists(results_path)
+    with open(results_path, "a") as file:
+        if write_header:
+            file.write("dataset,noise,epsilon,target_model,attack_model,accuracy\n")
+
+
+    noise_param = list(it.product(
+        ["data", "algorithm", "output"], # where noise was added
+        epsilons
+    ))
+    ml_models = ["random_forest", "naive_bayes", "logistic_regression"]
+
+    for noise in tqdm(["none"] + noise_param, desc="Noise"):
+        if noise == "none":
+            ep = None
+        else:
+            noise, ep = noise
+            ep = f"{ep:.1f}"
+
+        for M_name in tqdm(ml_models, desc="Target Model", leave=False):
+            # Load M
+            try:
+                with open(
+                    os.path.join(RESULTS_PATH, f"target_models/{M_name}/{dataset}_{noise}_eps_{ep}.pkl"), 'rb'
+                ) as file:
+                    M = pickle.load(file)
+
+                for A_name in tqdm(ml_models, desc="Attack Model", leave=False):
+                    try:
+                        # Load A
+                        with open(
+                            os.path.join(RESULTS_PATH, f"attack_models/{M_name}_{dataset}_{noise}_eps_{ep}_{A_name}_attack.pkl"), 'rb'
+                        ) as file:
+                            A = pickle.load(file)
+
+                        try:
+                            acc = model_inversion_acc(
+                                dataset_path=dataset_path,
+                                qids=qids,
+                                target=target,
+                                sensitive=sensitive,
+                                M=M,
+                                A=A,
+                                dp_output=(noise == "output"),
+                                epsilon=ep
+                            )
+                        except Exception as e:
+                            print(f"Error at {dataset},{noise},{ep},{M_name},{A_name}\nError: {e}\n")  
+                        
+                        with open(results_path, "a") as file:
+                            file.write(f"{dataset},{noise},{ep},{M_name},{A_name},{acc:.10f}\n")
+                    except Exception as e:
+                        print(f"Error loading A: {dataset},{noise},{ep},{M_name},{A_name}\nError: {e}\n")
+            except Exception as e:
+                print(f"Error loading M: {dataset},{noise},{ep},{M_name}\nError: {e}\n")
 
 def main():
-    # DATASET INFO #####################################
-    # Adult
-    adult_dataset_path = os.path.join(REPOSITORY_PATH, "data/adult/adult_pp.csv")
-    adult_qids = ["age", "workclass", "occupation", "sex", "education", "native-country", "marital-status"]
-    adult_target = "income"
-    adult_sensitive = "race"
-    adult_domain_sensitive = ["Asian-Pac-Islander", "Amer-Indian-Eskimo", "Black", "Other", "White"]
-    
-    # Hospitals
-    hospitals_dataset_path = os.path.join(REPOSITORY_PATH, "data/hospitals/hospitals_pp.csv")
-    hospitals_qids = ["TYPE_OF_ADMISSION", "PAT_ZIP", "PAT_COUNTY", "PAT_STATUS", "SEX_CODE", "RACE", "ADMIT_WEEKDAY", "PAT_AGE"]
-    hospitals_target = "TOTAL_CHARGES"
-    hospitals_sensitive = "PRINC_DIAG_CODE"
-    hospitals_domain_sensitive = ['M1711', 'M1712', 'M1612', 'M1611', 'T814XXA', 'E6601', 'L03116', 'G7281', 'I69354', 'I4891', 'R5381', 'I69351', 'J441', 'J209', 'G459', 'J9621', 'A0472', 'A419', 'E1169', 'I132', 'K5720', 'J9600', 'L03115', 'E11621', 'I639', 'N390', 'J690', 'N10', 'E1152', 'J9622', 'J9601', 'M48061', 'I214', 'J189', 'T83511A', 'I110', 'J101', 'J440', 'I2699', 'N179', 'A4189', 'K529', 'I480', 'J1000', 'F319', 'F250', 'F332', 'F209', 'F333', 'F322', 'F3481', 'I130', 'I350', 'K922', 'K8590', 'J45901', 'R0789', 'E860', 'E871', 'K56609', 'S72142A', 'I2510', 'R55', 'K5732', 'J159', 'O6981X0', 'Z3801', 'O34219', 'O76', 'O700', 'O34211', 'Z3800', 'O134', 'O80', 'O99824', 'K56600', 'O365930', 'O701', 'J210', 'K8000', 'Z3831', 'K3580', 'I25110', 'E8770', 'E1110', 'E1010', 'O9902', 'O321XX0', 'I160', 'A4151', 'I120', 'O480', 'E875', 'O1414', 'R079', 'O4103X0', 'O6014X0', 'Z5111', 'O4202', 'O4292']
-    ####################################################
-    
-    # TRAINING DATASET: ORIGINAL #######################
-    # Load target models
-    M_trained_original = []
-
-    # Load attack models
-    A_trained_original = []
-    
-    # Adult
-    for dp in [False,True]:
-        attack(
-            results_path=os.path.join(RESULTS_PATH, "attack_adult_original.csv"),
-            dataset_path=adult_dataset_path,
-            qids=adult_qids,
-            target=adult_target,
-            sensitive=adult_sensitive,
-            domain_sensitive=adult_domain_sensitive,
-            all_M=M_trained_original,
-            all_A=A_trained_original,
-            dp=dp
-        )
-
-    # Hospitals
-    for dp in [False,True]:
-        attack(
-            results_path=os.path.join(RESULTS_PATH, "attack_hospitals_original.csv"),
-            dataset_path=hospitals_dataset_path,
-            qids=hospitals_qids,
-            target=hospitals_target,
-            sensitive=hospitals_sensitive,
-            domain_sensitive=hospitals_domain_sensitive,
-            all_M=M_trained_original,
-            all_A=A_trained_original,
-            dp=dp
-        )
-    ####################################################
-
-    # TRAINING DATASET: SANITIZED WITH LDP #############
-    # Load target models
-    M_trained_ldp = []
-
-    # Load attack models
-    A_trained_ldp = []
-
-    # Adult
+    # Adult dataset
+    log("Evaluating privacy on adult dataset")
     attack(
-        results_path=os.path.join(RESULTS_PATH, "attack_adult_ldp.csv"),
-        dataset_path=adult_dataset_path,
-        qids=adult_qids,
-        target=adult_target,
-        sensitive=adult_sensitive,
-        domain_sensitive=adult_domain_sensitive,
-        all_M=M_trained_ldp,
-        all_A=A_trained_ldp,
-        dp=False
+        dataset="adult",
+        qids=["age", "workclass", "occupation", "sex", "education", "native-country", "marital-status"],
+        target="income",
+        sensitive="race"
+    )
+    
+    # Hospitals
+    log("Evaluating privacy on hospitals dataset")
+    attack(
+        dataset="hospitals",
+        qids=["TYPE_OF_ADMISSION", "PAT_ZIP", "PAT_COUNTY", "PAT_STATUS", "SEX_CODE", "RACE", "ADMIT_WEEKDAY", "PAT_AGE"],
+        target="TOTAL_CHARGES",
+        sensitive="PRINC_DIAG_CODE"
     )
 
-    # Hospitals
-    attack(
-        results_path=os.path.join(RESULTS_PATH, "attack_hospitals_ldp.csv"),
-        dataset_path=hospitals_dataset_path,
-        qids=hospitals_qids,
-        target=hospitals_target,
-        sensitive=hospitals_sensitive,
-        domain_sensitive=hospitals_domain_sensitive,
-        all_M=M_trained_ldp,
-        all_A=A_trained_ldp,
-        dp=False
-    )
-    ####################################################
+    log("Finished experiment")
 
 if __name__ == "__main__":
     main()
--- scripts/privacy_evaluation.py
+++ scripts/privacy_evaluation.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from mechanisms import krr
 from tqdm.auto import tqdm
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.compose import ColumnTransformer
 import sys
 import os
 
@@ -41,16 +43,36 @@ def make_guess(predictions:list, confidences:list[float]):
     # Choose randomly one value from the argmax set
     return np.random.choice(candidates)
 
+def process_x(X:pd.DataFrame):
+    # Create preprocessors
+    categorical_cols = X.select_dtypes(include=['object']).columns
+    numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', StandardScaler(), numerical_cols),
+            ('cat', OneHotEncoder(drop='first', sparse_output=False, handle_unknown='ignore'), categorical_cols)
+        ])
+    preprocessor.fit(X)
+    X_processed = preprocessor.transform(X)
+    return X_processed
+
+def process_y(y:pd.Series):
+    # Fit preprocessors on original data
+    label_encoder = LabelEncoder()
+    label_encoder.fit(y)
+    y_processed = label_encoder.transform(y)
+    return label_encoder, y_processed
+
 def model_inversion_acc(
         dataset_path:str,
         qids:list[str],
         target:str,
         sensitive:str,
-        domain_sensitive:list,
         M,
         A,
-        dp=False,
-        epsilon=math.log(2)
+        dp_output=False,
+        epsilon=None
     ):
     """Adversary's accuracy when trying to guess the sensitive attribute value. The evaluation is done considering all individuals in the original dataset as possible targets. The accuracy is the number of individuals the adversary guessed correctly the sensitive attribute using the attack model.
 
@@ -59,49 +81,63 @@ def model_inversion_acc(
         qids (list[str]): List of quasi-identifiers.
         target (str): Target attribute for the machine learning model.
         sensitive (str): Sensitive attribute.
-        domain_sensitive (list): Domain of the sensitive attribute.
         M: Target model. It must implement the method 'predict' that returns, for each instance, an array of confidences (same order as 'domain_sensitive').
         A: Attack model. It must implement the method 'predict' that returns, for each instance, an array of confidences (same order as 'domain_sensitive').
-        dp (bool): Whether to apply noise in the prediction of M. The noise is going to be added using kRR mechanism and the privacy level is passed through parameter `epsilon`.Default is False.
-        epsilon (float): Privacy parameter. Is is used only when `dp`=True.
+        dp_output (bool): Whether to apply noise in the prediction of M. The noise is going to be added using kRR mechanism and the privacy level is passed through parameter `epsilon`.Default is False.
+        epsilon (float): Privacy parameter. Is is used only when `dp_output`=True.
 
     Returns:
         accuracy (float): Adversary's accuracy.
     """
-
     # Load original dataset (pre-processed)
     data_ori = pd.read_csv(dataset_path)
+    domain_sensitive = data_ori[sensitive].unique().tolist()
+    n = len(data_ori) # Number of individuals
 
-    # Get the list of predictions for the target attribute
+    X_M = data_ori[qids].copy()
+    X_M = X_M.loc[X_M.index.repeat(len(domain_sensitive))] # Duplicate each individual k k = |domain(sensitive)| times
+    X_M[sensitive] = domain_sensitive * n # Add all possible sensitive values to each duplicate of each individual
+    
+    # Process the fetaures to be in the same format as the model
+    X_M = process_x(X_M)
+
+    y_M_conf = M.predict_proba(X_M)
+    y_M_pred = np.argmax(y_M_conf, axis=1)
+
+    if dp_output:
+        if isinstance(epsilon, str):
+            epsilon = float(epsilon)
+        y_M_pred = [krr(c, M.classes_, epsilon) for c in y_M_pred]
+
+    # Get the predictions to the original domain
+    label_encoder, _ = process_y(data_ori[target])
+    y_M_pred = label_encoder.inverse_transform(y_M_pred)
+
+    # STEP 4 #############
+    # Get predictions and confidences for the sensitive attribute
+    
+    # Build the dataset to pass through A
+    X_A = data_ori[qids].copy()
+    X_A = X_A.loc[X_A.index.repeat(len(domain_sensitive))] # Duplicate each individual k = |domain(sensitive)| times
+    X_A[target] = y_M_pred # Predictions from M
+
+    X_A = process_x(X_A)
+    y_A_conf = A.predict_proba(X_A)
+    y_A_pred = np.argmax(y_A_conf, axis=1)
+
+    # Get the predictions to the original domain
+    label_encoder, _ = process_y(data_ori[sensitive])
+    y_A_pred = label_encoder.inverse_transform(y_A_pred)
+
     correct_guesses = 0
-    for i in np.arange(len(data_ori)):
-        individual_qids = data_ori.loc[i][qids].tolist() # Individual's QID
-        individual_sensitive = data_ori.loc[i][sensitive] # Individual's real sensitive value
-
-        # STEP 3 #############
-        # Build tuples of (qids,sensitive) to be passed through M to get the target predictions
-        features_prediction = [individual_qids + [value] for value in domain_sensitive]
-        features_prediction = pd.DataFrame(features_prediction, columns=qids + [sensitive])
-        target_confidences = M.predict(features_prediction)
-        target_predictions = [domain_sensitive[np.argmax(c)] for c in target_confidences]
-        
-        if dp:
-            target_predictions = [krr(c, domain_sensitive, epsilon) for c in target_predictions]
-
-        # Build the dataset to pass through A
-        features_attack = features_prediction[qids] # QID values
-        features_attack[target] = target_predictions # Predictions from M
-
-        # STEP 4 #############
-        # Get predictions and confidences for the sensitive attribute
-        sensitive_confidences = A.predict(features_attack)
-        sensitive_predictions = [domain_sensitive[np.argmax(c)] for c in sensitive_confidences]
-
-        # Make the guess and check whether the adversary was correct
-        guess = make_guess(sensitive_predictions, sensitive_confidences)
-        if guess == individual_sensitive:
+    for i in np.arange(n):
+        y_A_conf_ind = y_A_conf[i*len(domain_sensitive):(i+1)*len(domain_sensitive)]
+        y_A_conf_ind = np.max(y_A_conf_ind, axis=1)
+        y_A_pred_ind = y_A_pred[i*len(domain_sensitive):(i+1)*len(domain_sensitive)]
+        y_original = data_ori.loc[i][sensitive]
+        guess = make_guess(y_A_pred_ind, y_A_conf_ind)
+        if guess == y_original:
             correct_guesses += 1
 
-    accuracy = correct_guesses/len(data_ori)
-
+    accuracy = correct_guesses/n
     return accuracy
